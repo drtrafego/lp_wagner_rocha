9075d4ab8ceb9ee355ba17a302b5d28f14f36275
DR.TRAFEGO <dr.trafego@gmail.com>
2026-05-11 17:57:44 -0300

    feat: Google Tag Manager GTM-MDG98V62

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,6 +83,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="pt-BR">
       <head>
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MDG98V62');` }} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
@@ -92,6 +93,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <JsonLd />
       </head>
       <body>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDG98V62" height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
         <MetaPixel />
         <GoogleAnalytics />
         {children}
